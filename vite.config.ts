d31a0003d31747ad099a7f12631be31b4f49e0ab
minhnhatdepzai <lenhat1232k2321@gmail.com>
2026-02-24 14:04:20 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: "/PromptTet/",
-      plugins: [react()],
+      base: '/PromptTet/',
       server: {
         port: 3000,
         host: '0.0.0.0',
